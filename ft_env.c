be62afd68ea2338b99feb426dc1c2455a33ac5b6
Bicheron baptiste <baptiste@bicheron.com>
2017-08-18 20:40:41 +0200

    2 new function, setenv, unsetenv

--- ft_env.c
+++ ft_env.c
@@ -1,62 +1,121 @@
 #include "mini.h"
 
+/*
+** ft_stock_env(), split environment line to name and value
+** and place them in a chained list, if PATH name is find
+** go stock path values
+*/
 void		ft_stock_env(t_env *list_env, t_path *path, char **envp)
 {
 	char	**env;
 	char	*var_env;
 	char	**split;
+	t_env	*env_list;
 
 	env = envp;
 	var_env = NULL;
+	env_list = list_env;
 	while (*env != 0)
 	{
 		var_env = *env;
 		split = ft_strsplit(var_env, '=');
-		list_env->name = *split;
+		env_list->name = *split;
 		if (ft_strcmp(*split++, "PATH") == 0)
 			ft_stock_path(path, *split);
-		list_env->value = *split;
-		if (env++)
+		env_list->value = *split;
+		env++;
+		if (*env)
 		{
-			list_env->next = (t_env *)malloc(sizeof(t_env));
-			list_env = list_env->next;
+			env_list->next = (t_env *)malloc(sizeof(t_env));
+			env_list = env_list->next;
 		}
 		else
-			list_env->next = NULL;
+			env_list->next = NULL;
 	}
 }
 
+/*
+** ft_stock_path(), take the full env variable path in arg
+** and split it to a chained list. Check if every directory
+** can be accessed.
+*/
 void		ft_stock_path(t_path *path, char *path_value)
 {
 	char	**split;
+	t_path	*path_list;
 
 	split = ft_strsplit(path_value, ':');
+	path_list = path;
 	while (*split != 0)
 	{
-		path->value = *split;
+		path_list->value = *split;
 		if (opendir(*split) != NULL)
-			path->valid = 1;
+			path_list->valid = 1;
 		else
-			path->valid = 0;
+			path_list->valid = 0;
 		if (split++)
 		{
-			path->next = (t_path *)malloc(sizeof(t_path));
-			path = path->next;
+			path_list->next = (t_path *)malloc(sizeof(t_path));
+			path_list = path_list->next;
 		}
 		else
-			path->next = NULL;
+			path_list->next = NULL;
 	}
 }
 
-int			main(int ac, char **av, char **envp)
+/*
+** ft_setenv(), permit to add of edit an environment variable
+** if env variable already exist, var is edited else it created
+*/
+void		ft_setenv(t_env *env, char *name, char *value)
 {
-	t_env		*list_env;
-	t_path		*path;
+	int	set;
+	t_env	*env_list;
 
-	ac = 0;
-	(void)av;
-	list_env = (t_env *)malloc(sizeof(t_env));	
-	path = (t_path *)malloc(sizeof(t_path));	
-	ft_stock_env(list_env, path, envp);
-	return (0);
+	set = 0;
+	env_list = env;
+	while (env_list)
+	{
+		if (ft_strcmp(env_list->name, name) == 0)
+		{
+			env_list->value = value;
+			set = 1;
+			break ;
+		}
+		if (!env_list->next)
+			break;
+		env_list = env_list->next;
+	}
+	if (set == 0)
+	{
+		env_list->next = (t_env *)malloc(sizeof(t_env));
+		env_list->next->name = name;
+		env_list->next->value = value;
+		env_list->next->next = NULL;
+	}
+}
+
+/*
+** ft_unsetenv(), take variable name in arg, and check if
+** it exist, if so, delete it
+*/
+void		ft_unsetenv(t_env *env, char *name)
+{
+	t_env	*prev;
+	t_env	*env_list;
+
+	prev = NULL;
+	env_list = env;
+	while (env_list)
+	{
+		if (ft_strcmp(env_list->name, name) == 0)
+		{
+			if (env_list->next)
+				prev->next = env_list->next;
+			else
+				prev->next = NULL;
+		}
+		prev = env_list;
+		env_list = env_list->next;
+	}
 }
--- mini.h
+++ mini.h
@@ -21,7 +21,9 @@ typedef struct		s_path
 	int		valid;
 }			t_path;
 
-void		ft_stock_path(t_path *path, char *path_value);
 void		ft_stock_env(t_env *list_env, t_path *path, char **envp);
+void		ft_stock_path(t_path *path, char *path_value);
+void		ft_setenv(t_env *env, char *name, char *value);
+void		ft_unsetenv(t_env *env, char *name);
 
 #endif
