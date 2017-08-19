50a20bae1235270ed8ad0380539724c0ee747ffb
Bicheron baptiste <baptiste@bicheron.com>
2017-08-19 17:18:42 +0200

    Add new struct for local variables

--- mini.h
+++ mini.h
@@ -8,37 +8,45 @@
 int         get_next_line(const int fd, char **line);
 int         ft_count_rows(char *name);
 
-typedef struct		s_env
+typedef struct			s_loc_var
 {
-	struct s_env	*next;
-	char		*name;
-	char		*value;
-}			t_env;
+	struct s_loc_var	*next;
+	char				*name;
+	char				*value;
+}						t_loc_var
 
-typedef struct		s_path
+typedef struct			s_env
 {
-	struct s_path	*next;
-	char		*value;
-	int		valid;
-}			t_path;
+	struct s_env		*next;
+	char				*name;
+	char				*value;
+}						t_env;
 
-typedef struct		s_group
+typedef struct			s_path
 {
-	t_env *env;
-	char *name;
-	char *value;
-}					t_group;
+	struct s_path		*next;
+	char				*value;
+	int					valid;
+}						t_path;
 
-typedef struct		s_built
+typedef struct			s_group
+{
+	t_env				*env;
+	t_loc_var			*loc_var;
+	char				*name;
+	char				*value;
+}						t_group;
+
+typedef struct			s_built
 {
 	char *list_built_in[7];
     void (*list_fct[7])(t_group *);
-}					t_built;
+}						t_built;
 
-void		ft_stock_env(t_env *list_env, t_path *path, char **envp);
-void		ft_stock_path(t_path *path, char *path_value);
-void		ft_setenv(t_env *env, char *name, char *value);
-void		ft_unsetenv(t_env *env, char *name);
-void		ft_print_env(t_group *group);
+void					ft_stock_env(t_env *list_env, t_path *path, char **envp);
+void					ft_stock_path(t_path *path, char *path_value);
+void					ft_setenv(t_env *env, char *name, char *value);
+void					ft_unsetenv(t_env *env, char *name);
+void					ft_print_env(t_group *group);
 
 #endif
